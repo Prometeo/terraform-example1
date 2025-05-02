84c1f490624e5fee518956d006eef0079d76a415
Prometeo <xairi.valdivia@gmail.com>
2025-05-01 23:53:06 -0500

    test creation

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+    cloud {
+        organization = "Development-2345"
+        workspaces {
+            name = "terraform-example1"
+        }
+    }
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.96"
+        }
+    }
+}
+
+
+
 provider "aws" {
-  region = var.region
+    region = var.region
 }
--- security-groups.tf
+++ security-groups.tf
@@ -13,13 +13,13 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_sh_ipv4" {
-  security_group_id = aws_security_group.allow_ssh.id
-  cidr_ipv4         = var.ssh_sg_cidr_block
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_sh_ipv4" {
+#   security_group_id = aws_security_group.allow_ssh.id
+#   cidr_ipv4         = var.ssh_sg_cidr_block
+#   from_port         = 22
+#   ip_protocol       = "tcp"
+#   to_port           = 22
+# }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ports" {
     for_each = { for port in var.ports : port => port }
